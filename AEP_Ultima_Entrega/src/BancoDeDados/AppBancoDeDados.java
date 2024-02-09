42907907ec3700991ea0cea17e936c464758a4ca
Guilherme Lopes <68930185+guilopesfeitosa@users.noreply.github.com>
2024-02-09 08:55:07 -0300

    Update AppBancoDeDados.java

--- AEP_Ultima_Entrega/src/BancoDeDados/AppBancoDeDados.java
+++ AEP_Ultima_Entrega/src/BancoDeDados/AppBancoDeDados.java
@@ -10,7 +10,7 @@ public class AppBancoDeDados {
 	
 	public static void main(String[] args) throws SQLException {
 		try {
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/AEP_Esoft4s2021","Guilherme","2013gui187");
+			Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/AEP_Esoft4s2021","Admin","Admin");
 			System.out.println("foi");
 		} catch (Exception e) {
 			e.printStackTrace();
